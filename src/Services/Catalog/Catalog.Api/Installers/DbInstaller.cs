0786d31e353b7cc8f04c6153cb35ab94a239a4f2
ShafiAhmedShaik <shafiahmed120@gmail.com>
2022-06-02 18:11:14 +0530

    Added entity and db settings and abstracted Installers

---
+++ src/Services/Catalog/Catalog.Api/Installers/DbInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Api.Utilities;
+
+namespace Catalog.Api.Installers
+{
+    public class DbInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<DbOptions>(configuration.GetSection("DatabaseSettings"));
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Utilities/DbOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Api.Utilities
+{
+    public class DbOptions
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ProductCollectionName { get; set; }
+    }
+}
